dacccc1e772098355c0350533041d7ee843c598b
mudboi <77167996+mudboi@users.noreply.github.com>
2023-12-16 22:55:57 -0600

    additional common functions

--- blender_import/modules/blender_auto_common.py
+++ blender_import/modules/blender_auto_common.py
@@ -1,4 +1,70 @@
 import bpy
+import ast
+import mathutils
+from collections import OrderedDict
+
+
+class RigifyLimbIKBakeSettings:
+    def __init__(self, prop_bone='', fk_bones='[]', ik_bones='[]', ctrl_bones='[]', tail_bones='[]', extra_ctrls='[]'):
+        self.prop_bone = prop_bone
+        self.fk_bones = fk_bones
+        self.ik_bones = ik_bones
+        self.ctrl_bones = ctrl_bones
+        self.tail_bones = tail_bones
+        self.extra_ctrls = extra_ctrls
+
+    def get_bone_flat_list(self):
+        out_list = []
+        out_list += [self.prop_bone]
+        out_list += self.parse_string_list(self.fk_bones)
+        out_list += self.parse_string_list(self.ik_bones)
+        out_list += self.parse_string_list(self.ctrl_bones)
+        out_list += self.parse_string_list(self.tail_bones)
+        out_list += self.parse_string_list(self.extra_ctrls)
+        return out_list
+
+    @staticmethod
+    def parse_string_list(parse_str):
+        try:
+            return ast.literal_eval(parse_str)
+        except SyntaxError as se:
+            raise Exception("Could not parse limb IK bake setting bone list") from se
+
+
+class EditBoneCopier:
+    """Class to copy edit bones attributes.
+
+    Attributes to copy are listed in the class variable 'attrs_to_copy'. Will fill
+    out an OrderedDict, 'ctrl_rig_attrs', with all attribute data from all bones passed to
+    'copy_edit_bone_data()' method, keys will be bone names, vals will be a dict with
+    attribute name: attribute value"""
+    attrs_to_copy = ['envelope_distance', 'envelope_weight', 'head', 'head_radius',
+                     'inherit_scale', 'tail', 'tail_radius', 'use_connect', 'roll',
+                     'use_deform', 'use_endroll_as_inroll', 'use_envelope_multiply',
+                     'use_inherit_rotation', 'use_local_location', 'use_relative_parent']
+
+    def __init__(self):
+        self.ctrl_rig_attrs = OrderedDict()
+
+    def copy_edit_bone_data(self, ctrl_bone):
+        """Copy edit bone data from 'ctrl_bone' and stores in 'ctrl_rig_attrs'.
+
+        'ctrl_bone' MUST be an edit bone, and passed in whatever order desired, this
+        class will keep track of that order through the use of an OrderedDict to
+        store the copied data.
+        """
+        self.ctrl_rig_attrs[ctrl_bone.name] = {}
+        if ctrl_bone.parent:  # Check if bone has parent and get name str
+            self.ctrl_rig_attrs[ctrl_bone.name]['parent'] = ctrl_bone.parent.name
+        else:
+            self.ctrl_rig_attrs[ctrl_bone.name]['parent'] = None
+        for at in self.attrs_to_copy:  # iter over attr_to_copy and copy values
+            if type(getattr(ctrl_bone, at)) is mathutils.Vector:
+                # Blender deallocates memory for vectors and matrices when leaving edit
+                #     mode, so need to make a copy to store
+                self.ctrl_rig_attrs[ctrl_bone.name][at] = getattr(ctrl_bone, at).copy()
+            else:
+                self.ctrl_rig_attrs[ctrl_bone.name][at] = getattr(ctrl_bone, at)
 
 
 def switch_to_mode(obj, mode, context=None):
@@ -37,7 +103,6 @@ def get_export_armature():
     """ Get first armature that is in a collection called 'Export' or None if one not found.
 
     This is useful for getting the send2ue armature that contains all the unreal export animations"""
-    export_col = None
     try:
         export_col = bpy.data.collections['Export']
     except KeyError:
@@ -47,9 +112,52 @@ def get_export_armature():
             return obj.data
     return None
 
+
 def push_action_to_nla(rig_obj):
     """Push 'rig_obj's active action to NLA"""
     act = rig_obj.animation_data.action  # Get current action
     track = rig_obj.animation_data.nla_tracks.new()  # create new NLA track and push action to it
     track.strips.new(act.name, int(act.frame_range[0]), act)
     rig_obj.animation_data.action = None  # Unlink the action as active action
+
+
+def toggle_constrain_bones_rig_to_rig(constrain, src_rig, constrain_rig, bones, verbose=True):
+    """Constrain or unconstrain the bones specified by bones in constrain_rig to corresponding bones in src_rig
+
+    Note: corresponding bones have to be named identical in both rigs. Removes all other bone constraints from
+    bones not used for constraining them"""
+    for bone_name in bones:
+        cnst_bone_ind = constrain_rig.pose.bones.find(bone_name)
+        src_bone_ind = src_rig.pose.bones.find(bone_name)
+        if cnst_bone_ind < 0 or src_bone_ind < 0:
+            if verbose:
+                print("Could not find bone: " + bone_name + " in  either constrain rig or source rig")
+            continue
+        if verbose:
+            toggle = "ON" if constrain else "OFF"
+            print("Toggling constraints " + toggle + " for: " + bone_name)
+        cnst_bone = constrain_rig.pose.bones[cnst_bone_ind]
+        src_bone = src_rig.pose.bones[src_bone_ind]
+        for cnst in cnst_bone.constraints:
+            if verbose:
+                print("    Removing constraint: " + cnst.type + ": " + cnst.name)
+            cnst_bone.constraints.remove(cnst)
+        loc = cnst_bone.constraints.new(type="COPY_LOCATION")
+        loc.target = src_rig
+        loc.subtarget = src_bone.name
+        loc.enabled = constrain
+        rot = cnst_bone.constraints.new(type="COPY_ROTATION")
+        rot.target = src_rig
+        rot.subtarget = src_bone.name
+        rot.enabled = constrain
+
+
+def traverse_bone_heirarchy(bn, operator, method):
+    """Recursively tranverse a bone heirarchy and perform operation on each bone.
+
+        Should pass in root bones as 'bn', function will recursively call itself on
+        all child bones. 'operator' is any object that has a method named 'method' to
+        call on every bone recursively"""
+    getattr(operator, method)(bn)
+    for ch in bn.children:
+        traverse_bone_heirarchy(ch, operator, method)
